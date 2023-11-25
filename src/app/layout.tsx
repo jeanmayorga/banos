fb7837fe39c1082a40c91e1b909a4f5621bd1a80
Jean Paul Mayorga <jpmayorga@outlook.com>
2023-11-24 21:12:29 -0500

    fix: updating page

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from "components";
+
+export default function Page() {
+  return (
+    <>
+      <Header />
+      <h1>Hello, Next.js!</h1>
+    </>
+  );
+}
