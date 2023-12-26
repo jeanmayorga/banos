9135c04869cab8353b713e2bdb87d682624ac303
Jean Paul Mayorga <jpmayorga@outlook.com>
2023-12-25 19:14:43 -0500

    fix: error

--- src/app/api/cloudinary/route.ts
+++ src/app/api/cloudinary/route.ts
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from "cloudinary";
+// import { v2 as cloudinary } from "cloudinary";
 
 export const runtime = "edge";
 
@@ -10,9 +10,9 @@ export async function DELETE(request: Request) {
     const file = path.split("/")[1];
     const public_id = file.split(".")[0];
 
-    const data = await cloudinary.uploader.destroy(public_id);
+    // const data = await cloudinary.uploader.destroy(public_id);
 
-    return Response.json({ data });
+    return Response.json({ data: "" });
   } catch (e: any) {
     console.log(`${e.message}`);
     return Response.json({ error: e.message }, { status: 500 });
