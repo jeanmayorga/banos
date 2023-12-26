ebcf487e40b989e32f900f2e5b2dccd6c8ea1a16
Jean Paul Mayorga <jpmayorga@outlook.com>
2023-12-26 00:01:16 -0500

    feat: adding pages

---
+++ src/app/places/page.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "#/components/ui/typography";
+
+export default function Page() {
+  return (
+    <>
+      <div className="container mx-auto py-16">
+        <Typography variant="h2" component="h1">
+          Lugares en Banos de agua santa Ecuador
+        </Typography>
+      </div>
+    </>
+  );
+}
