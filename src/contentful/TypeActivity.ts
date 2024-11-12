77e168bc7704bef0c4ef63bbdd00de7540beb81a
Jean Paul Mayorga <jpmayorga@outlook.com>
2024-11-12 07:39:02 -0500

    fix: update

--- src/contentful/TypeActivity.ts
+++ src/contentful/TypeActivity.ts
@@ -1,19 +1,29 @@
-import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode } from "contentful";
+import type {
+  ChainModifiers,
+  Entry,
+  EntryFieldTypes,
+  EntrySkeletonType,
+  LocaleCode,
+} from "contentful";
+
 import type { TypePlacesSkeleton } from "./TypePlaces";
 
 export interface TypeActivityFields {
-    slug: EntryFieldTypes.Symbol;
-    title: EntryFieldTypes.Symbol;
-    seoKeywords: EntryFieldTypes.Symbol;
-    seoDescription: EntryFieldTypes.Symbol;
-    description: EntryFieldTypes.RichText;
-    images: EntryFieldTypes.Array<EntryFieldTypes.AssetLink>;
-    place: EntryFieldTypes.EntryLink<TypePlacesSkeleton>;
-    location?: EntryFieldTypes.Location;
-    youtubeVideo?: EntryFieldTypes.Symbol;
-    adultPrice: EntryFieldTypes.Number;
-    childPrice: EntryFieldTypes.Integer;
+  slug: EntryFieldTypes.Symbol;
+  title: EntryFieldTypes.Symbol;
+  seoKeywords: EntryFieldTypes.Symbol;
+  seoDescription: EntryFieldTypes.Symbol;
+  description: EntryFieldTypes.RichText;
+  images: EntryFieldTypes.Array<EntryFieldTypes.AssetLink>;
+  place: EntryFieldTypes.EntryLink<TypePlacesSkeleton>;
+  location?: EntryFieldTypes.Location;
+  youtubeVideo?: EntryFieldTypes.Symbol;
+  adultPrice: EntryFieldTypes.Number;
+  childPrice: EntryFieldTypes.Integer;
 }
 
 export type TypeActivitySkeleton = EntrySkeletonType<TypeActivityFields, "activity">;
-export type TypeActivity<Modifiers extends ChainModifiers, Locales extends LocaleCode = LocaleCode> = Entry<TypeActivitySkeleton, Modifiers, Locales>;
+export type TypeActivity<
+  Modifiers extends ChainModifiers,
+  Locales extends LocaleCode = LocaleCode,
+> = Entry<TypeActivitySkeleton, Modifiers, Locales>;
